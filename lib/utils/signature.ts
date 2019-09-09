4141b5c15621042843371b8763f8584d39db7763
Danko Adamczyk <danko@czyk.nl>
2019-09-09 13:22:27 +0200

    Use base64 atm for signature

--- lib/utils/signature.ts
+++ lib/utils/signature.ts
@@ -1,9 +1,9 @@
 export function generateSignature(secret: string): string{
     // TODO(@czyk): how to sign etc?
-    return secret;
+    return Buffer.from(secret).toString('base64');
 }
 
 export function validateSignature(secret, signature): boolean {
     // TODO(@czyk): how to validate etc?
-    return secret === signature;
+    return generateSignature(secret) === signature;
 }
